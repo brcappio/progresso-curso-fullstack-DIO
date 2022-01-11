8ccf6127892fe374730e5b6294d8fae261d709b2
cappio <brunocappio@gmail.com>
2022-01-10 23:41:13 -0300

    Meu primeiro contador feito com JavaScript

---
+++ introdução-javascript/assets/js/scripts.js
@@ -0,0 +1,31 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber >= 0) {
+        document.getElementById("currentNumber").style.color = "black";
+    }
+    if (currentNumber >= 10) {
+        document.getElementById("mais").disabled = true;
+    } else {
+        document.getElementById("mais").disabled = false;
+    }
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0) {
+        document.getElementById("currentNumber").style.color = "red";
+    }
+    if (currentNumber <= -10) {
+        document.getElementById("menos").disabled = true;
+    } else {
+        document.getElementById("menos").disabled = false;
+    }
+}
+
+mais.addEventListener("click", increment)
+menos.addEventListener("click", decrement)
